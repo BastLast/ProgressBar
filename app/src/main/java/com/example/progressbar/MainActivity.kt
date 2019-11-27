97fab794375dc0052cfd0fe26c25fa1dd05cda77
Bast <bastien.last87@gmail.com>
2019-11-27 10:32:01 +0100

    Premier tests de progressbar

--- app/src/main/java/com/example/progressbar/MainActivity.kt
+++ app/src/main/java/com/example/progressbar/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.progressbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var id = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progress_horizontal.setProgress(0)
+
+        plusBtn.setOnClickListener {
+            progress_horizontal.setProgress( ++ id)
+        }
+
+        minusBtn.setOnClickListener {
+            progress_horizontal.setProgress( -- id)
+        }
     }
 }
