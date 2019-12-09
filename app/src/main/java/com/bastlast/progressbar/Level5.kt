7b76be5959964aa4d33a4bd739a09cb2c3d72037
Bast <bastien.last87@gmail.com>
2019-12-09 14:30:13 +0100

    Ajout d'un niveau

--- app/src/main/java/com/bastlast/progressbar/Level5.kt
+++ app/src/main/java/com/bastlast/progressbar/Level5.kt
@@ -1,12 +1,41 @@
 package com.bastlast.progressbar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_splasscreen.*
 
 class Level5 : AppCompatActivity() {
-
+    private var id = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_level5)
+        Thread(Runnable
+        {
+            while (id <= 100) {
+                id = 0;
+                progress_horizontal.setProgress(id, true);
+                try {
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }).start()
+    }
+
+    fun add(view: View) {
+        id += 20
+        progress_horizontal.setProgress(id, true);
+        if (id >= 100) {
+            gotoLevel6()
+        }
+    }
+
+    private fun gotoLevel6() {
+        val level6Intent = Intent(applicationContext, Level6::class.java)
+        startActivity(level6Intent)
+        finish()
     }
 }
