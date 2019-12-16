09e5b8ac2e24b5df7e813c0f40a52ce4996a09d9
Bast <bastien.last87@gmail.com>
2019-12-16 15:10:38 +0100

    Ajout d'un niveau

--- app/src/main/java/com/bastlast/progressbar/Level11.kt
+++ app/src/main/java/com/bastlast/progressbar/Level11.kt
@@ -1,12 +1,68 @@
 package com.bastlast.progressbar
 
+import android.content.Context
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_level11.*
 
-class Level11 : AppCompatActivity() {
+class Level11 : AppCompatActivity(), SensorEventListener {
+
+    private var accelerometer: Sensor? = null
+    private lateinit var sensorManager: SensorManager
+    private var light: Sensor? = null
+    private var id = 0
+    private var echelle = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_level11)
+        // get reference of the service
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        // focus in accelerometer
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
     }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null) {
+            calculateprogressbarvalue(event)
+            progress_horizontal.setProgress(id, true);
+            if (id >= 100) {
+                gotoLevel12()
+            }
+        }
+    }
+
+    private fun calculateprogressbarvalue(event: SensorEvent) {
+        val position = event.values[1]*10
+        id = position.toInt()
+    }
+
+    override fun onResume() {
+        // Register a listener for the sensor.
+        super.onResume()
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    private fun gotoLevel12() {
+        val level12Intent = Intent(applicationContext, Level12::class.java)
+        startActivity(level12Intent)
+        finish()
+    }
+
 }
