dd79012bfe41f6b85c2f8a7ef32229d475948d79
Bast <bastien.last87@gmail.com>
2019-12-27 01:02:42 +0100

    BugFix

--- app/src/main/java/com/bastlast/progressbar/MainActivity.kt
+++ app/src/main/java/com/bastlast/progressbar/MainActivity.kt
@@ -37,8 +37,9 @@ class MainActivity : AppCompatActivity() {
             10 -> Intent(applicationContext, Level10::class.java)
             11 -> Intent(applicationContext, Level11::class.java)
             12 -> Intent(applicationContext, Level12::class.java)
-            else -> Intent(applicationContext, Level13::class.java)
-            //else -> Intent(applicationContext, Tempend::class.java)
+            13 -> Intent(applicationContext, Level13::class.java)
+            //else -> Intent(applicationContext, Level13::class.java)
+            else -> Intent(applicationContext, Tempend::class.java)
         }
     }
 
